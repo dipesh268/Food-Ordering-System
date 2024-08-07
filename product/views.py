0ef9d04195724732a66297dff376e2cbece77088
DipesH Mundhara <dipeshmundhara09@gmail.com>
2024-08-07 21:05:35 +0530

    create database and admin panel.

--- product/views.py
+++ product/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render,redirect
 from product.models import *
 from django.contrib import messages
+from product.models import customer
 from django.contrib.auth import authenticate, login,logout
+from django.contrib.auth.models import User
 # Create your views here.
 
 
@@ -12,18 +14,19 @@ def home(request):
 def login_page(request):
     if request.method == "POST":
         customer_email = request.POST.get('Email')
-        password = request.POST.get('password')
-        print(password)
+        pwd = request.POST.get('pwd')
+        print(pwd)
         print(customer_email)
         
         if customer.objects.filter(customer_email=customer_email).exists():
-            user = authenticate(customer_email=customer_email, password=password)
-            if user is None:
-                messages.info(request, "Invalid password.")
-                return redirect('/login/')
-            else:
+            user = customer.objects.filter(password = pwd)
+            print(user)
+            if user:
                 login(request,user)
                 return redirect('/index/')
+            else:
+                messages.info(request, "Invalid password.")
+                return redirect('/login/')
         else:
             messages.info(request, "Invalid Username.")
             return redirect('/login/')
